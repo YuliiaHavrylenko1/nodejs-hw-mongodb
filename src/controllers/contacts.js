7dd43b97767aac3ffef93763bb598bd6ad648594
YuliiaHavrylenko1 <gavrilenko.j94@gmail.com>
2025-10-21 19:02:42 +0100

    HW3: CRUD complete

---
+++ src/controllers/contacts.js
@@ -0,0 +1,53 @@
+
+import createError from 'http-errors';
+
+import {
+  getAllContacts,
+  getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
+} from '../services/contacts.js';
+
+export const getAllContactsCtrl = async (req, res) => {
+  const response = await getAllContacts();
+  res.status(response.status).json(response);
+};
+
+export const getContactByIdCtrl = async (req, res) => {
+  const contactId = req.params.contactId;
+  const response = await getContactById(contactId);
+
+  if (response.status === 404) {
+    throw createError(404, 'Contact not found');
+  }
+
+  res.status(response.status).json(response);
+};
+
+export const createContactCtrl = async (req, res) => {
+  const response = await createContact(req.body);
+  res.status(201).json(response);
+};
+
+export const updateContactCtrl = async (req, res) => {
+  const contactId = req.params.contactId;
+  const response = await updateContact(contactId, req.body);
+
+  if (response.status === 404) {
+    throw createError(404, 'Contact not found');
+  }
+
+  res.status(response.status).json(response);
+};
+
+export const deleteContactCtrl = async (req, res) => {
+  const contactId = req.params.contactId;
+  const response = await deleteContact(contactId);
+
+  if (response.status === 404) {
+    throw createError(404, 'Contact not found');
+  }
+
+  res.status(204).end();
+};
